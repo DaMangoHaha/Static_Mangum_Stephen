5bf8d81d65a1e0583221c6f6683dab526fa0f42b
DaMangoHaha <stephenmangum01@gmail.com>
2025-04-11 08:05:02 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Static_Mangum_Stephen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
